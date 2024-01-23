2f005f3ac8d0491a2ca166c920b2083174ed231e
emlesna4002 <anselmepena2004@gmail.com>
2024-01-23 13:21:39 +0100

    ajout d'une machine a état dans le game.c Suppression du programme test dans le main.c

--- src/game.c
+++ src/game.c
@@ -1,6 +1,45 @@
+#include "raylib.h"
 #include "game.h"
+#include "home.h"
+#include "menu.h"
+#include "map.h"
+#include "settings.h"
+#include "end_game.h"
+
+static enum{stat_home, stat_menu, stat_settings, stat_end_game} stat=stat_home;
 
 void game(void)
 {
-    return;
+
+    switch (stat)
+    {
+    case stat_home:
+        home();
+        break;
+
+    case stat_menu:
+        menu();
+        break;
+
+    case stat_settings:
+        settings();
+        break;
+
+    case stat_end_game:
+        end_game();
+        break;
+    
+    default:
+        break;
+    }
+
+
 }
+
+// void DrawGame(void){
+
+//     BeginDrawing();
+//         DrawMap();
+//     EndDrawing();
+
+// }
