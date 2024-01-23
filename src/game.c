131e471c2eb0ba76fc64f58696d52631c0e6e3eb
emlesna4002 <anselmepena2004@gmail.com>
2024-01-23 15:48:25 +0100

    ajout menu

--- src/game.c
+++ src/game.c
@@ -1,8 +1,8 @@
-#include "raylib.h"
 #include "game.h"
+#include <stdio.h>
 
 State previous_state = STATE_HOME;
-State state = STATE_HOME;
+State state = STATE_MENU;
 State next_state = STATE_HOME;
 
 void game(void)
@@ -12,23 +12,31 @@ void game(void)
     case STATE_HOME:
         next_state = UpdateHome();
         DrawHome();
+        printf("home");
         break;
 
     case STATE_MENU:
-        menu();
+        next_state = UpdateMenu();
+        DrawMenu();
+        printf("menu");
         break;
 
     case STATE_SETTINGS:
         settings();
+        printf("set");
         break;
 
     case STATE_END_GAME:
         end_game();
+        printf("end");
         break;
     
     default:
         break;
     }
+
+    
+
     previous_state = state;
     state = next_state;
 }
