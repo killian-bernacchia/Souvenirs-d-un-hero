02f6fc7bb778608f08d4e7cd765a47cfa2a53cfd
Gatien <rinceventmage@gmail.com>
2024-01-24 08:00:10 +0100

    dialogue amelioré

--- src/dialogs.c
+++ src/dialogs.c
@@ -11,18 +11,19 @@ void UpdateDialogueGuardien(){
     indexGuardien ++;
 }
 
-void drawDialogueGuardien(){
-    char* guardien [5] = {"alte qui êtes vous ame en peine  ?" , "Et a qui avons nous l’honneur ?" ,"Et bien encore une âme qui essaye de s'infiltrer dans le panthéons","J’ai déjà entendue parler de cette légende ou certaine âme perdre leur mémoire durant le trajet et que les bribes de cette mémoire se retrouve éparpillée au 4 coins du chant des châtiments.", "Oui, si la légende dit vrai, mais prends garde : le chant des châtiments est rempli de créatures, en particulier les Éthérombres, dont il faut se méfier. Ils voudront aspirer ton esprit jusqu'à la dernière once de vie. Bonne chance, âme perdue."};
+void drawDialogueGuardien(){    
+    
+    Font font = LoadFont("resources/Coors Script Regular.ttf");
 
-    Rectangle play_btn = {300, 500, 446, 300};
-    DrawRectangleRec(play_btn, BLACK);
-    DrawText(guardien[indexGuardien], 400, 600, 48, WHITE);
-}
+    char* guardien [5] = {"alte qui êtes vous ame en peine  ?" , "Et a qui avons nous l’honneur ?" ,"Et bien encore une âme qui essaye de s'infiltrer dans le panthéons","J’ai déjà entendue parler de cette légende ou certaine âme perdre leur mémoire durant le trajet et\nque les bribes de cette mémoire se retrouve éparpillée au 4 coins du chant des châtiments.", "Oui, si la légende dit vrai, mais prends garde :\n le chant des châtiments est rempli de créatures, en particulier les Éthérombres, dont il faut se méfier\n Ils voudront aspirer ton esprit jusqu'à la dernière once de vie. Bonne chance, âme perdue."};
+
+    DrawText(guardien[indexGuardien], 25, 660, 25, BLACK);
+
+    // DrawTextEx(font, guardien[indexGuardien], (Vector2){ 25.0f, 660.0f }, (float)25, 2, MAROON);
+    }
 
 void drawDialogueFeuFollet(){
     char* feuxfollet [5] = {"un hero légendaire venant prendre repos." , "Et Bien je suis …  Je suis … Je suis qui ? je ne sais plus" ,"Mais je ne vous mens pas j’ai réellement perdue la mémoire comment puis-je prouver ma fois ?", "je n’aurais qu’as donc retrouver ces brides et ma mémoire reviendras ?", "j’y veillerais."};
 
-    Rectangle play_btn = {300, 500, 446, 300};
-    DrawRectangleRec(play_btn, BLACK);
-    DrawText(feuxfollet[indexfeuxfollet], 400, 600, 48, WHITE);
+    DrawText(feuxfollet[indexfeuxfollet], 25, 660, 25, BLACK);
 }
